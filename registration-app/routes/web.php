b2a4db9b6298ca999f941c27acf155d88c87a893
Vanessa Munson <vanessa.munson03@gmail.com>
2024-09-18 00:08:21 +0200

    Update Subject route

--- registration-app/routes/web.php
+++ registration-app/routes/web.php
@@ -2,10 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController;
+use App\Http\Controllers\SubjectController;
 use Inertia\Inertia;
 
 Route::get('/', [StudentController::class, 'index']);
-
 Route::resource('students', StudentController::class)->except('index');
+
+Route::get('/subjects', [SubjectController::class, 'index']);
+Route::resource('subjects', SubjectController::class)->except('index');
 
 // Route::inertia('/', 'Home');
