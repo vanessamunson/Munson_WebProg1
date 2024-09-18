1a5fe4b3abe3b043b623b633189f6fdc09e5cf68
Vanessa Munson <vanessa.munson03@gmail.com>
2024-09-18 12:32:46 +0200

    Renamed Home to Student

---
+++ registration-app/resources/js/Pages/Student.jsx
@@ -0,0 +1,28 @@
+import { Link } from "@inertiajs/react";
+
+export default function Student({students}) {
+    return (
+        <>
+            <h1>Hello</h1>
+
+            <div>
+                <table>
+                    <tr>
+                        <th>Name</th>
+                        <th>Race</th>
+                        <th>Hall</th>
+                        <th>Department</th>
+                    </tr>
+                    {students.map(student => (
+                        <tr key={student.id}>
+                            <td>{student.name}</td>
+                            <td>{student.race}</td>
+                            <td>{student.hall}</td>
+                            <td>{student.department_id}</td>
+                        </tr>
+                    ))}
+                </table>
+            </div>
+        </>
+    )
+}
