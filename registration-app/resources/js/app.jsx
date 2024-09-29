31ef8da4dd7d0135af971f6106e395674c8868f7
Vanessa Munson <vanessa.munson03@gmail.com>
2024-09-29 22:08:31 +0200

    Changed to Nav.jsx

--- registration-app/resources/js/app.jsx
+++ registration-app/resources/js/app.jsx
@@ -3,13 +3,13 @@ import '../css/app.css'
 
 import { createInertiaApp } from '@inertiajs/react'
 import { createRoot } from 'react-dom/client'
-import Layout from './Layouts/Layout';
+import Nav from './Layouts/Nav';
 
 createInertiaApp({
   resolve: name => {
     const pages = import.meta.glob('./Pages/**/*.jsx', { eager: true });
     let page = pages[`./Pages/${name}.jsx`];
-    page.default.layout = page.default.layout || ((page) => <Layout children={page}/>);
+    page.default.layout = page.default.layout || ((page) => <Nav children={page}/>);
     return page;
   },
   setup({ el, App, props }) {
