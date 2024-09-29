684dad5eb78576f1e7219e4c0431439a1c8ab36a
Vanessa Munson <vanessa.munson03@gmail.com>
2024-09-29 22:10:40 +0200

    Made table pretty

--- registration-app/resources/js/Pages/Subject.jsx
+++ registration-app/resources/js/Pages/Subject.jsx
@@ -3,20 +3,24 @@ import { Link } from "@inertiajs/react";
 export default function Subject({subjects}) {
     return (
         <>
-            <div>
-                <table>
-                    <tr>
-                        <th>Name</th>
-                        <th>Department</th>
-                        <th>Description</th>
-                    </tr>
+            <div class="relative overflow-x-auto shadow-md sm:rounded-lg" >
+                <table className="table-auto w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                    <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                        <tr>
+                            <th scope="col" className="px-6 py-3">Name</th>
+                            <th scope="col" className="px-6 py-3">Department</th>
+                            <th scope="col" className="px-6 py-3">Description</th>
+                        </tr>
+                    </thead>
+                    <tbody>
                     {subjects.data.map(subject => (
-                        <tr key={subject.id}>
-                            <td>{subject.name}</td>
-                            <td>{subject.department_id}</td>
-                            <td>{subject.description}</td>
+                        <tr key={subject.id} className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+                            <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">{subject.name}</td>
+                            <td className="px-6 py-4">{subject.department_id}</td>
+                            <td className="px-6 py-4">{subject.description}</td>
                         </tr>
                     ))}
+                    </tbody>
                 </table>
             </div>
 
