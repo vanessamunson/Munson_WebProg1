89adc5fc6aa936a224e22af5d64e18bcdf7c1e8b
Vanessa Munson <vanessa.munson03@gmail.com>
2024-12-02 18:57:10 +0100

    Use subject table data

--- my-app/database/factories/SubjectFactory.php
+++ my-app/database/factories/SubjectFactory.php
@@ -17,7 +17,14 @@ class SubjectFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => $this->faker->word(),
+            'level_id' => $this->faker->numberBetween(1, 5),
+            'dept_id' => $this->faker->numberBetween(1, 6),
+            'credits' => $this->faker->numberBetween(1, 10),
+            'description' => $this->faker->sentence(),
+            'obligatory' => $this->faker->boolean(),
+            'facultative' => $this->faker->boolean(),
+            'lecturer_id' => $this->faker->numberBetween(1, 11)
         ];
     }
 }
